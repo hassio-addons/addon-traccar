17c090a97b6e600b03191250ff50ddec9f69c89d
Franck Nijhof <frenck@addons.community>
2019-03-31 18:31:23 +0200

    :hammer: Rewrite add-on onto Bashio

--- traccar/rootfs/etc/cont-init.d/20-config.sh
+++ traccar/rootfs/etc/cont-init.d/20-config.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: Traccar
 # Ensures the user configuration file is present
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
-if ! hass.file_exists "/config/traccar.xml"; then
+if ! bashio::fs.file_exists "/config/traccar.xml"; then
     cp /etc/traccar/traccar.xml /config/traccar.xml
 fi
