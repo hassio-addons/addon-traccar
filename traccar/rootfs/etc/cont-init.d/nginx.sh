f3b00f4e10b9f5911304d5d48ce7f111d1542bee
Franck Nijhof <frenck@addons.community>
2019-06-23 23:49:15 +0200

    :sparkles: Adds Ingress support

--- traccar/rootfs/etc/cont-init.d/nginx.sh
+++ traccar/rootfs/etc/cont-init.d/nginx.sh
@@ -3,22 +3,33 @@
 # Community Hass.io Add-ons: Traccar
 # Configures NGINX for use with the Traccar server
 # ==============================================================================
-declare certfile
-declare keyfile
 declare port
+declare certfile
+declare ingress_interface
+declare ingress_port
+declare keyfile
 
-bashio::config.require.ssl
+port=$(bashio::addon.port 80)
+if bashio::var.has_value "${port}"; then
+    bashio::config.require.ssl
 
-if bashio::config.true 'ssl'; then
-    rm /etc/nginx/nginx.conf
-    mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
+    if bashio::config.true 'ssl'; then
+        certfile=$(bashio::config 'certfile')
+        keyfile=$(bashio::config 'keyfile')
 
-    certfile=$(bashio::config 'certfile')
-    keyfile=$(bashio::config 'keyfile')
+        mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
+        sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/servers/direct.conf
+        sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/servers/direct.conf
 
-    sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx.conf
-    sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx.conf
+    else
+        mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
+    fi
+
+    sed -i "s/%%port%%/${port}/g" /etc/nginx/servers/direct.conf
 fi
 
-port=$(bashio::addon.port 8072)
-sed -i "s/%%port%%/${port}/g" /etc/nginx/nginx.conf
+ingress_port=$(bashio::addon.ingress_port)
+ingress_interface=$(bashio::addon.ip_address)
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+
