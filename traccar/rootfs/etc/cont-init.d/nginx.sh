25f2dc9e8658deaec38bb9b1204f475b571e1917
Franck Nijhof <frenck@addons.community>
2019-08-24 10:07:26 +0200

    :hammer: Use Hass.io DNS as NGinx resolver

--- traccar/rootfs/etc/cont-init.d/nginx.sh
+++ traccar/rootfs/etc/cont-init.d/nginx.sh
@@ -5,6 +5,7 @@
 # ==============================================================================
 declare port
 declare certfile
+declare hassio_dns
 declare ingress_interface
 declare ingress_port
 declare keyfile
@@ -33,3 +34,5 @@ ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 
+hassio_dns=$(bashio::dns.host)
+sed -i "s/%%hassio_dns%%/${hassio_dns}/g" /etc/nginx/includes/resolver.conf
