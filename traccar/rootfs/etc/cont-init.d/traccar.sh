61469fcf054f824ba408b67a4f736e58d6434d03
Franck Nijhof <git@frenck.dev>
2023-11-10 22:13:46 +0100

    Migrate Traccar config out of Home Assistant config folder (#261)

--- traccar/rootfs/etc/cont-init.d/traccar.sh
+++ traccar/rootfs/etc/cont-init.d/traccar.sh
@@ -8,6 +8,14 @@ declare password
 declare port
 declare username
 
+# Migrate add-on data from the Home Assistant config folder,
+# to the add-on configuration folder.
+if ! bashio::fs.directory_exists '/config/traccar.xml' \
+    && bashio::fs.file_exists '/homeassistant/traccar.xml'; then
+    mv /homeassistant/traccar.xml /config/traccar.xml \
+        || bashio::exit.nok "Failed to migrate Traccar configuration"
+fi
+
 if ! bashio::fs.file_exists "/config/traccar.xml"; then
     cp /etc/traccar/traccar.xml /config/traccar.xml
 else
